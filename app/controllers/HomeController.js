8828c446f0c1330f69e028a513a566acd0d3ca26
Thanh BInh <technobia@gmail.com>
2015-11-18 21:36:31 +0700

    update link on home

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -4,15 +4,21 @@
 define(function(require) {
     'use strict';
 
-    function HomeController($scope) {
+    function HomeController($scope, $rootScope) {
         this.data = $scope.data = {};
         this.event = $scope.event = {};
+        this.$rootScope = $rootScope;
 
         this.event.onLoad = this.onLoad.bind(this);
+        this.event.updateSelected = this.publishUpdateSelected.bind(this);
     }
 
     HomeController.prototype.onLoad = function() {
 
+    };
+
+    HomeController.prototype.publishUpdateSelected = function(number) {
+        this.$rootScope.$broadcast('updateSelected', number);
     };
 
     return HomeController;
